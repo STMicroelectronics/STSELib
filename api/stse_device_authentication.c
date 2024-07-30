3a04cb0428f04c2c7ab7bed5212af196c1f1cc19
Christophe Delaunay <christophe.delaunay@st.com>
2024-07-30 13:22:52 +0200

    [api] Pass zone argument to api stse_device_authenticate, stse_get_device_certificate, stse_get_device_certificate_size & stse_get_device_id

--- api/stse_device_authentication.c
+++ api/stse_device_authentication.c
@@ -24,18 +24,17 @@
 #include "certificate/stse_certificate_subparsing.h"
 #include "certificate/stse_certificate_crypto.h"
 
-#define STSAFE_CERTIFICATE_ZONE_0 		0U
-#define CERTIFICATE_SIZE_OFFSET_BYTES 	2U
-#define CERTIFICATE_SIZE_LENGTH 		2U
-#define CERTIFICATE_OFFSET_BYTES		0U
-#define CERTIFICATE_DEVICE_ID_OFFSET	15U
-#define CERTIFICATE_DEVICE_ID_SIZE		11U
+#define STSE_CERTIFICATE_SIZE_OFFSET_BYTES 	2U
+#define STSE_CERTIFICATE_SIZE_LENGTH 		2U
+#define STSE_CERTIFICATE_OFFSET_BYTES		0U
+#define STSE_CERTIFICATE_DEVICE_ID_OFFSET	15U
+#define STSE_CERTIFICATE_DEVICE_ID_SIZE		11U
 
 /* Static functions declaration ----------------------------------------------*/
 
 /* Exported functions --------------------------------------------------------*/
 
-stse_ReturnCode_t stse_get_device_id(stse_Handler_t * pSTSE, PLAT_UI8* device_id)
+stse_ReturnCode_t stse_get_device_id(stse_Handler_t * pSTSE, PLAT_UI8 certificate_zone, PLAT_UI8* pDevice_id)
 {
 	volatile stse_ReturnCode_t ret = STSE_API_INVALID_PARAMETER;
 
@@ -47,17 +46,17 @@ stse_ReturnCode_t stse_get_device_id(stse_Handler_t * pSTSE, PLAT_UI8* device_id
 
 	ret = stse_data_storage_read_data_zone(
 			pSTSE,							/* STSAFE handler */
-			STSAFE_CERTIFICATE_ZONE_0,		/* Zone = 0 */
-			CERTIFICATE_DEVICE_ID_OFFSET,	/* X bytes offset */
-			device_id,						/* Returned certificate size */
-			CERTIFICATE_DEVICE_ID_SIZE,		/* Certificate size length */
-			0,								/* No maximum chunck size (No chunck at all) */
-			STSAFEA_NO_PROT);				/* No protection */
+			certificate_zone,				/* Certificate zone */
+			STSE_CERTIFICATE_DEVICE_ID_OFFSET,	/* X bytes offset */
+			pDevice_id,						/* Returned certificate size */
+			STSE_CERTIFICATE_DEVICE_ID_SIZE,		/* Certificate size length */
+			0,										/* No maximum chunck size (No chunck at all) */
+			STSAFEA_NO_PROT);						/* No protection */
 
 	return ret;
 }
 
-stse_ReturnCode_t stse_get_device_certificate_size(stse_Handler_t * pSTSE, PLAT_UI16 * certificate_size)
+stse_ReturnCode_t stse_get_device_certificate_size(stse_Handler_t * pSTSE, PLAT_UI8 certificate_zone, PLAT_UI16 * pCertificate_size)
 {
 	volatile stse_ReturnCode_t ret = STSE_API_INVALID_PARAMETER;
 	PLAT_UI8 certificate_size_ui8[2];
@@ -69,27 +68,31 @@ stse_ReturnCode_t stse_get_device_certificate_size(stse_Handler_t * pSTSE, PLAT_
 	}
 
 
+	if (pCertificate_size == NULL)
+	{
+		return ( STSE_API_INVALID_PARAMETER );
+	}
+
 	ret = stse_data_storage_read_data_zone(
-			pSTSE,							/* STSAFE handler */
-			STSAFE_CERTIFICATE_ZONE_0,		/* Zone = 0 */
-			CERTIFICATE_SIZE_OFFSET_BYTES,	/* 2 bytes offset */
-			certificate_size_ui8,			/* Returned certificate size */
-			CERTIFICATE_SIZE_LENGTH,		/* Certificate size length 2 bytes */
-			0,								/* No maximum chunck size (No chunck at all) */
-			STSAFEA_NO_PROT);				/* No protection */
+			pSTSE,								/* STSAFE handler */
+			certificate_zone,					/* Certificate zone */
+			STSE_CERTIFICATE_SIZE_OFFSET_BYTES,	/* 2 bytes offset */
+			certificate_size_ui8,				/* Returned certificate size */
+			STSE_CERTIFICATE_SIZE_LENGTH,		/* Certificate size length 2 bytes */
+			0,									/* No maximum chunck size (No chunck at all) */
+			STSAFEA_NO_PROT);					/* No protection */
 
 	if(ret != STSE_OK)
 	{
 		return( ret );
 	}
 
-	*certificate_size = (((uint16_t)certificate_size_ui8[0]) << 8) + ((uint16_t)certificate_size_ui8[1]) + 4U;
-
+	*pCertificate_size = (((uint16_t)certificate_size_ui8[0]) << 8) + ((uint16_t)certificate_size_ui8[1]) + 4U;
 
 	return STSE_OK;
 }
 
-stse_ReturnCode_t stse_get_device_certificate(stse_Handler_t * pSTSE, PLAT_UI16 certificate_size, PLAT_UI8* stsafe_certificate)
+stse_ReturnCode_t stse_get_device_certificate(stse_Handler_t * pSTSE, PLAT_UI8 certificate_zone, PLAT_UI16 certificate_size, PLAT_UI8* pCertificate)
 {
 	volatile stse_ReturnCode_t ret = STSE_API_INVALID_PARAMETER;
 
@@ -99,14 +102,19 @@ stse_ReturnCode_t stse_get_device_certificate(stse_Handler_t * pSTSE, PLAT_UI16 
 		return( STSE_API_HANDLER_NOT_INITIALISED );
 	}
 
+	if (pCertificate == NULL)
+	{
+		return ( STSE_API_INVALID_PARAMETER );
+	}
+
 	ret = stse_data_storage_read_data_zone(
-			pSTSE,							/* STSAFE handler */
-			STSAFE_CERTIFICATE_ZONE_0,		/* Zone = 0 */
-			CERTIFICATE_OFFSET_BYTES,		/* 0 bytes offset */
-			stsafe_certificate,				/* Returned certificate size */
-			certificate_size,				/* Certificate size length */
+			pSTSE,								/* STSAFE handler */
+			certificate_zone,					/* Certificate zone */
+			STSE_CERTIFICATE_OFFSET_BYTES,		/* 0 bytes offset */
+			pCertificate,						/* Returned certificate size */
+			certificate_size,					/* Certificate size length */
 			255,								/* No maximum chunck size (No chunck at all) */
-			STSAFEA_NO_PROT);				/* No protection */
+			STSAFEA_NO_PROT);					/* No protection */
 
 	if(ret != STSE_OK)
 	{
@@ -119,18 +127,53 @@ stse_ReturnCode_t stse_get_device_certificate(stse_Handler_t * pSTSE, PLAT_UI16 
 stse_ReturnCode_t stse_device_authenticate(
 		stse_Handler_t * pSTSE,
 		const PLAT_UI8 *pRoot_CA_certificate,
-		const PLAT_UI8 *pCertificate_chain,
-		PLAT_UI16 certificate_chain_size,
+		PLAT_UI8 certificate_zone,
 		PLAT_UI8 priv_key_slot_number)
 {
 	stse_ReturnCode_t ret;
 	stse_certificate_t leaf_certificate;
+	PLAT_UI16 certificate_size;
+	PLAT_UI8 certificate_size_ui8[2];
 
+	if (pSTSE == NULL)
+	{
+		return( STSE_API_HANDLER_NOT_INITIALISED );
+	}
+
+	if (pRoot_CA_certificate == NULL)
+	{
+		return ( STSE_API_INVALID_PARAMETER );
+	}
+
+	ret = stse_data_storage_read_data_zone(
+			pSTSE,
+			certificate_zone,
+			STSE_CERTIFICATE_SIZE_OFFSET_BYTES,	/* 2 bytes offset */
+			certificate_size_ui8,				/* Returned certificate size */
+			STSE_CERTIFICATE_SIZE_LENGTH,		/* Certificate size length 2 bytes */
+			0,									/* No maximum chunck size (No chunck at all) */
+			STSAFEA_NO_PROT);					/* No protection */
+
+	if(ret != STSE_OK)
+	{
+		return ret;
+	}
+
+	certificate_size = (((uint16_t)certificate_size_ui8[0]) << 8) + ((uint16_t)certificate_size_ui8[1]) + 4U;
+
+	PLAT_UI8 certificate[certificate_size];
+
+	ret = stse_get_device_certificate(pSTSE, certificate_zone, certificate_size, certificate);
+	if (ret != STSE_OK)
+	{
+		return ret;
+
+	}
 
 	ret = stse_certificate_parse_chain(
 			(uint8_t*)pRoot_CA_certificate,
-			(uint8_t*)pCertificate_chain,
-			certificate_chain_size,
+			certificate,
+			certificate_size,
 			&leaf_certificate);
 
 	if (ret != STSE_OK)
@@ -178,5 +221,6 @@ stse_ReturnCode_t stse_device_authenticate(
 	{
 		return STSE_API_INVALID_SIGNATURE;
 	}
+
 	return STSE_OK;
 }
