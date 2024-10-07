50ca693ea5c803b561e5ab5af20b6e7e4d76c0c8
Joshua WOLFCARIUS <joshua.wolfcarius@st.com>
2024-10-07 14:34:57 +0200

    [Services] Add STSAFE-L010 device info services

---
+++ services/stsafel/stsafel_device_info.c
@@ -0,0 +1,127 @@
+/*!
+ ******************************************************************************
+ * \file	stsafel_device_info.c
+ * \brief   Device info services for STSAFE-L
+ * \author  STMicroelectronics - CS application team
+ *
+ ******************************************************************************
+ * \attention
+ *
+ * <h2><center>&copy; COPYRIGHT 2024 STMicroelectronics</center></h2>
+ *
+ * This software is licensed under terms that can be found in the LICENSE file in
+ * the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+
+stse_ReturnCode_t stsafel_get_device_UID(
+		stse_Handler_t * pSTSE,
+		PLAT_UI8 * pDevice_UID)
+{
+	return stsafel_get_data(
+		pSTSE,
+		STSAFEL_INFO_UNIQUE_IDENTIFIER,
+		0,
+		NULL,
+		STSAFEL_UID_LENGTH,
+		pDevice_UID);
+}
+
+stse_ReturnCode_t stsafel_get_device_traceability(
+		stse_Handler_t * pSTSE,
+		stsafel_device_traceability_t * pDevice_traceability)
+{
+	return stsafel_get_data(
+		pSTSE,
+		STSAFEL_INFO_TRACEABILITY_DATA,
+		0,
+		NULL,
+		sizeof(stsafel_device_traceability_t),
+		(PLAT_UI8*)pDevice_traceability);
+}
+
+stse_ReturnCode_t stsafel_get_data(
+		stse_Handler_t * pSTSE,
+		stsafel_device_info_tag_t tag,
+		PLAT_UI16 additional_data_length,
+		PLAT_UI8 * pAdditional_data,
+		PLAT_UI16 device_info_expected_length,
+		PLAT_UI8 * pDevice_info)
+{
+	stse_ReturnCode_t ret;
+	PLAT_UI8 cmd_header = STSAFEL_CMD_GET_DATA;
+	PLAT_UI8 rsp_header;
+
+	if(pSTSE == NULL)
+	{
+		return STSE_SERVICE_HANDLER_NOT_INITIALISED;
+	}
+
+	if((pAdditional_data == NULL && additional_data_length != 0) ||
+	   (pDevice_info == NULL))
+	{
+		return STSE_SERVICE_INVALID_PARAMETER;
+	}
+
+	/*- Create CMD frame and populate elements */
+	stse_frame_allocate(CmdFrame);
+	stse_frame_element_allocate_push(&CmdFrame,eCmd_header,1,&cmd_header);
+	stse_frame_element_allocate_push(&CmdFrame,eTag,1,(PLAT_UI8*)&tag);
+	stse_frame_element_allocate_push(&CmdFrame,eAdditional_data,additional_data_length,pAdditional_data);
+
+	/*- Create Rsp frame and populate elements*/
+	stse_frame_allocate(RspFrame);
+	stse_frame_element_allocate_push(&RspFrame,eRsp_header,1,&rsp_header);
+	stse_frame_element_allocate_push(&RspFrame,eDevice_info,device_info_expected_length,pDevice_info);
+
+	/*- Perform Transfer*/
+	ret = stse_frame_transfer(pSTSE,
+			&CmdFrame,
+			&RspFrame,
+			stsafel_cmd_exec_duration(pSTSE, STSAFEL_CMD_GET_DATA)
+	);
+
+	return( ret );
+}
+
+stse_ReturnCode_t stsafel_put_data(
+		stse_Handler_t * pSTSE,
+		stsafel_device_info_tag_t tag,
+		PLAT_UI16 device_info_length,
+		PLAT_UI8 * pDevice_info)
+{
+	stse_ReturnCode_t ret;
+	PLAT_UI8 cmd_header = STSAFEL_CMD_PUT_DATA;
+	PLAT_UI8 rsp_header;
+
+	if(pSTSE == NULL)
+	{
+		return STSE_SERVICE_HANDLER_NOT_INITIALISED;
+	}
+
+	if(pDevice_info == NULL)
+	{
+		return STSE_SERVICE_INVALID_PARAMETER;
+	}
+
+	/*- Create CMD frame and populate elements */
+	stse_frame_allocate(CmdFrame);
+	stse_frame_element_allocate_push(&CmdFrame,eCmd_header,1,&cmd_header);
+	stse_frame_element_allocate_push(&CmdFrame,eTag,1,(PLAT_UI8*)&tag);
+	stse_frame_element_allocate_push(&CmdFrame,eDevice_info,device_info_length,pDevice_info);
+
+	/*- Create Rsp frame and populate elements*/
+	stse_frame_allocate(RspFrame);
+	stse_frame_element_allocate_push(&RspFrame,eRsp_header,1,&rsp_header);
+
+	/*- Perform Transfer*/
+	ret = stse_frame_transfer(pSTSE,
+			&CmdFrame,
+			&RspFrame,
+			stsafel_cmd_exec_duration(pSTSE, STSAFEL_CMD_GET_DATA)
+	);
+
+	return( ret );
+}
---
+++ services/stsafel/stsafel_device_info.h
@@ -0,0 +1,126 @@
+/*!
+ ******************************************************************************
+ * \file	stsafel_device_info.h
+ * \brief   Device info services for STSAFE-L
+ * \author  STMicroelectronics - CS application team
+ *
+ ******************************************************************************
+ * \attention
+ *
+ * <h2><center>&copy; COPYRIGHT 2024 STMicroelectronics</center></h2>
+ *
+ * This software is licensed under terms that can be found in the LICENSE file in
+ * the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+
+#ifndef STSAFEL_DEVICE_INFO_H
+#define STSAFEL_DEVICE_INFO_H
+
+#include "core/stse_return_codes.h"
+#include "core/stse_device.h"
+#include "core/stse_platform.h"
+#include "core/stse_util.h"
+#include "core/stse_frame.h"
+#include "services/stsafel/stsafel_commands.h"
+#include "services/stsafel/stsafel_timings.h"
+
+/*! \defgroup stsafel_device_info STSAFE-L device info
+ *  \ingroup stsafel_services
+ *  @{
+ */
+
+#define STSAFEL_UID_LENGTH 8U
+#define STSAFEL_CPSN_LENGTH 7U
+#define STSAFEL_HARDWARE_ID_LENGTH 2U
+#define STSAFEL_BOOT_ROM_ID_LENGTH 2U
+#define STSAFEL_SOC_ID_LENGTH 3U
+#define STSAFEL_TRACEABILITY_RESERVED_LENGTH 8U
+#define STSAFEL_DATA_CONFIGURATION_IDENTIFIER_LENGTH 8U
+
+typedef enum stsafel_device_info_tag_t{
+	STSAFEL_INFO_TRACEABILITY_DATA = 0x00,
+	STSAFEL_INFO_UNIQUE_IDENTIFIER,
+	STSAFEL_INFO_LIFE_CYCLE_STATE,
+	STSAFEL_INFO_KILL_COMMAND_CONFIGURATION,
+	STSAFEL_INFO_SWITCH_PROTOCOL_FLAG,
+	STSAFEL_INFO_ZONE_META_DATA_V1,
+	STSAFEL_INFO_ZONE_META_DATA_V2,
+	STSAFEL_INFO_STAGE_1_DATA_CONFIGURATION_IDENTIFIER = 0x0C,
+	STSAFEL_INFO_STAGE_2_DATA_CONFIGURATION_IDENTIFIER,
+	STSAFEL_INFO_STAGE_3_DATA_CONFIGURATION_IDENTIFIER
+} stsafel_device_info_tag_t;
+
+typedef struct /*!< Device traceability info */
+{
+	struct {
+		PLAT_UI8 hardware_id[STSAFEL_HARDWARE_ID_LENGTH];
+		PLAT_UI8 boot_rom_id[STSAFEL_BOOT_ROM_ID_LENGTH];
+		PLAT_UI8 soc_id[STSAFEL_SOC_ID_LENGTH];
+		PLAT_UI8 data_config_id;
+	} product_type;
+	PLAT_UI8 cpsn[STSAFEL_CPSN_LENGTH]; /*!< Chip Serial Number */
+	PLAT_UI8 reserved[STSAFEL_TRACEABILITY_RESERVED_LENGTH];
+} stsafel_device_traceability_t;
+
+/**
+ * \brief 		Get target device unique identifier
+ * \details 	This service format and send the get UID command
+ * \param[in] 	pSTSE 				Pointer to STSE Handler
+ * \param[out]	pDevice_UID			Device unique identifier
+ * \return \ref stse_ReturnCode_t : STSE_OK on success ; error code otherwise
+ */
+stse_ReturnCode_t stsafel_get_device_UID(
+		stse_Handler_t * pSTSE,
+		PLAT_UI8 * pDevice_UID);
+
+/**
+ * \brief 		Get target device traceability info
+ * \details 	This service format and send the get traceability info command
+ * \param[in] 	pSTSE 					Pointer to STSE Handler
+ * \param[out]	pDevice_traceability	Device traceability info
+ * \return \ref stse_ReturnCode_t : STSE_OK on success ; error code otherwise
+ */
+stse_ReturnCode_t stsafel_get_device_traceability(
+		stse_Handler_t * pSTSE,
+		stsafel_device_traceability_t * pDevice_traceability);
+
+/**
+ * \brief 		Get target device info
+ * \details 	This service format and send the get device info command
+ * \param[in] 	pSTSE                       Pointer to STSE Handler
+ * \param[in]	tag                         Tag of the data to get
+ * \param[in]	additional_data_length      Additional data length
+ * \param[in]	pAdditional_data            Additional data relative to requested info
+ * \param[in]	device_info_expected_length	Expected device info length
+ * \param[out]	pDevice_info			    Device info output buffer
+ * \return \ref stse_ReturnCode_t : STSE_OK on success ; error code otherwise
+ */
+stse_ReturnCode_t stsafel_get_data(
+		stse_Handler_t * pSTSE,
+		stsafel_device_info_tag_t tag,
+		PLAT_UI16 additional_data_length,
+		PLAT_UI8 * pAdditional_data,
+		PLAT_UI16 device_info_expected_length,
+		PLAT_UI8 * pDevice_info);
+
+/**
+ * \brief 		Put target device info
+ * \details 	This service format and send the put device info command
+ * \param[in] 	pSTSE                       Pointer to STSE Handler
+ * \param[in]	tag                         Tag of the data to put
+ * \param[in]	device_info_length	        Device info length
+ * \param[in]	pDevice_info			    Device info buffer
+ * \return \ref stse_ReturnCode_t : STSE_OK on success ; error code otherwise
+ */
+stse_ReturnCode_t stsafel_put_data(
+		stse_Handler_t * pSTSE,
+		stsafel_device_info_tag_t tag,
+		PLAT_UI16 device_info_length,
+		PLAT_UI8 * pDevice_info);
+
+/** \}*/
+
+#endif /* STSAFEL_DEVICE_INFO_H */
