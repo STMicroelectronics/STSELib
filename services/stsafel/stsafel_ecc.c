ecc352c3d77a6f35eabf41f35d5897eed3565ea8
Joshua WOLFCARIUS <joshua.wolfcarius@st.com>
2024-10-11 16:59:17 +0200

    [Services] Add STSAFE-L010 ECC service

---
+++ services/stsafel/stsafel_ecc.c
@@ -0,0 +1,67 @@
+/*!
+ ******************************************************************************
+ * \file	stsafel_ecc.h
+ * \brief   Elliptic Curves Cryptography (ECC) services for STSAFE-L
+ * \author  STMicroelectronics - CS application team
+ *
+ ******************************************************************************
+ * \attention
+ *
+ * <h2><center>&copy; COPYRIGHT 2024 STMicroelectronics</center></h2>
+ *
+ * This software is licensed under terms that can be found in the LICENSE file in
+ * the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+
+#include "services/stsafel/stsafel_ecc.h"
+#include "services/stsafel/stsafel_timings.h"
+
+
+stse_ReturnCode_t stsafel_ecc_generate_signature(
+		stse_Handler_t * pSTSE,
+		stse_ecc_key_type_t key_type,
+		PLAT_UI8 *pChallenge,
+		PLAT_UI16 challenge_length,
+		PLAT_UI8 *pSignature)
+{
+	stse_ReturnCode_t ret;
+	PLAT_UI8 cmd_header = STSAFEL_CMD_GENERATE_SIGNATURE;
+	PLAT_UI8 rsp_header;
+	PLAT_UI8 internal_data_subject = STSAFEL_ECC_SIGNATURE_SUBJECT_NONE;
+
+	if(pSTSE == NULL)
+	{
+		return STSE_SERVICE_HANDLER_NOT_INITIALISED;
+	}
+
+	if((key_type != STSE_ECC_KT_ED25519) ||
+	   (challenge_length != STSAFEL_ECC_SIGNATURE_CHALLENGE_LENGTH) ||
+	   (pChallenge == NULL) ||
+	   (pSignature == NULL))
+	{
+		return STSE_SERVICE_INVALID_PARAMETER;
+	}
+
+	/*- Create CMD frame and populate elements */
+	stse_frame_allocate(CmdFrame);
+	stse_frame_element_allocate_push(&CmdFrame,eCmd_header,1,&cmd_header);
+	stse_frame_element_allocate_push(&CmdFrame,eChallenge,challenge_length,pChallenge);
+	stse_frame_element_allocate_push(&CmdFrame,eInternal_data_subject,1,&internal_data_subject);
+
+	/*- Create Rsp frame and populate elements*/
+	stse_frame_allocate(RspFrame);
+	stse_frame_element_allocate_push(&RspFrame,eRsp_header,1,&rsp_header);
+	stse_frame_element_allocate_push(&RspFrame,eSignature,stse_ecc_info_table[key_type].signature_size,pSignature);
+
+	/*- Perform Transfer*/
+	ret = stse_frame_transfer(pSTSE,
+			&CmdFrame,
+			&RspFrame,
+			stsafel_cmd_exec_duration(pSTSE, STSAFEL_CMD_GENERATE_SIGNATURE)
+	);
+
+	return( ret );
+}
---
+++ services/stsafel/stsafel_ecc.h
@@ -0,0 +1,58 @@
+/*!
+ ******************************************************************************
+ * \file	stsafel_ecc.h
+ * \brief   Elliptic Curves Cryptography (ECC) services for STSAFE-L
+ * \author  STMicroelectronics - CS application team
+ *
+ ******************************************************************************
+ * \attention
+ *
+ * <h2><center>&copy; COPYRIGHT 2024 STMicroelectronics</center></h2>
+ *
+ * This software is licensed under terms that can be found in the LICENSE file in
+ * the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+
+#ifndef STSAFEL_ECC_H
+#define STSAFEL_ECC_H
+
+#include "core/stse_return_codes.h"
+#include "core/stse_device.h"
+#include "core/stse_platform.h"
+#include "core/stse_util.h"
+#include "core/stse_frame.h"
+#include "services/stsafel/stsafel_commands.h"
+#include "services/stsafel/stsafel_timings.h"
+
+/*! \defgroup stsafel_ecc STSAFE-L Elliptic Curves Cryptography (ECC) services
+ *  \ingroup stsafel_services
+ *  @{
+ */
+
+#define STSAFEL_ECC_SIGNATURE_CHALLENGE_LENGTH 16U
+#define STSAFEL_ECC_SIGNATURE_SUBJECT_TRACEABILITY 0xFE
+#define STSAFEL_ECC_SIGNATURE_SUBJECT_NONE 0xFF
+
+/**
+ * \brief 		Generate ECC signature over a challenge
+ * \details 	This service format and send generate signature command
+ * \param[in] 	pSTSE 				Pointer to STSE Handler
+ * \param[in] 	key_type 			\ref stse_ecc_key_type_t enum key type to use
+ * \param[in] 	challenge_length 	Challenge length
+ * \param[in] 	pChallenge 			Challenge to be signed
+ * \param[out]	pSignature			Signature output buffer
+ * \return \ref stse_ReturnCode_t : STSE_OK on success ; error code otherwise
+ */
+stse_ReturnCode_t stsafel_ecc_generate_signature(
+		stse_Handler_t * pSTSE,
+		stse_ecc_key_type_t key_type,
+		PLAT_UI8 *pChallenge,
+		PLAT_UI16 challenge_length,
+		PLAT_UI8 *pSignature);
+
+/** \}*/
+
+#endif /* STSAFEL_ECC_H */
