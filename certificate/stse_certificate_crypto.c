fdbe39170bc91040e3c827685047a2ec853a6c90
Jerome SEMETTE <jerome.semette@st.com>
2025-06-04 17:13:21 +0200

    [stsafea] apply clang format using pre-commit

--- certificate/stse_certificate_crypto.c
+++ certificate/stse_certificate_crypto.c
@@ -44,31 +44,31 @@ stse_ReturnCode_t stse_certificate_verify_cert_signature(const stse_certificate_
         digestPtr = (PLAT_UI8 *)child->tbs;
         digestSize = child->tbsSize;
         ret = STSE_OK;
-        }
+    }
 #ifdef STSE_CONF_USE_COMPANION
-	else if (stsafe_x509_parser_companion_handler != NULL &&
-		 stsafe_x509_parser_companion_handler->device_type == STSAFE_A120
+    else if (stsafe_x509_parser_companion_handler != NULL &&
+             stsafe_x509_parser_companion_handler->device_type == STSAFE_A120
 #ifdef STSE_CONF_HASH_SHA_256
-		 && hash_algo >= STSE_SHA_256
+             && hash_algo >= STSE_SHA_256
 #endif
-    ){ /* Only STSAFE-A120 support Hash features */
-		ret = stse_compute_hash(stsafe_x509_parser_companion_handler, hash_algo,
-					(PLAT_UI8 *)child->tbs, child->tbsSize, digestPtr,
-					(PLAT_UI16 *)&digestSize);
-	}
+    ) { /* Only STSAFE-A120 support Hash features */
+        ret = stse_compute_hash(stsafe_x509_parser_companion_handler, hash_algo,
+                                (PLAT_UI8 *)child->tbs, child->tbsSize, digestPtr,
+                                (PLAT_UI16 *)&digestSize);
+    }
 #endif
-	else {
-		ret = stse_platform_hash_compute(hash_algo, (PLAT_UI8 *)child->tbs, child->tbsSize,
-						 digestPtr, &digestSize);
-	}
+    else {
+        ret = stse_platform_hash_compute(hash_algo, (PLAT_UI8 *)child->tbs, child->tbsSize,
+                                         digestPtr, &digestSize);
+    }
 
-	if (ret != STSE_OK) {
-		return (ret);
-	}
+    if (ret != STSE_OK) {
+        return (ret);
+    }
 
-	return (stse_certificate_verify_signature(parent, digestPtr, digestSize, child->Sign.pR,
-						  child->Sign.rSize, child->Sign.pS,
-						  child->Sign.sSize));
+    return (stse_certificate_verify_signature(parent, digestPtr, digestSize, child->Sign.pR,
+                                              child->Sign.rSize, child->Sign.pS,
+                                              child->Sign.sSize));
 }
 
 stse_ReturnCode_t stse_certificate_verify_signature(const stse_certificate_t *cert,
@@ -78,150 +78,150 @@ stse_ReturnCode_t stse_certificate_verify_signature(const stse_certificate_t *ce
                                                     PLAT_I32 signatureRsize,
                                                     const PLAT_UI8 *signatureS,
                                                     PLAT_I32 signatureSsize) {
-        (void)signatureRsize;
-        (void)signatureSsize;
-        stse_ReturnCode_t ret;
-        stse_ecc_key_type_t key_type;
+    (void)signatureRsize;
+    (void)signatureSsize;
+    stse_ReturnCode_t ret;
+    stse_ecc_key_type_t key_type;
 
-        if ((cert == NULL) || (digest == NULL) || (signatureR == NULL) || (signatureS == NULL)) {
-            return (STSE_CERT_INVALID_PARAMETER);
-        }
+    if ((cert == NULL) || (digest == NULL) || (signatureR == NULL) || (signatureS == NULL)) {
+        return (STSE_CERT_INVALID_PARAMETER);
+    }
 
-        key_type = stse_certificate_get_key_type(cert);
-        if (key_type == STSE_ECC_KT_INVALID) {
-            return (STSE_CERT_INVALID_CERTIFICATE);
-        }
+    key_type = stse_certificate_get_key_type(cert);
+    if (key_type == STSE_ECC_KT_INVALID) {
+        return (STSE_CERT_INVALID_CERTIFICATE);
+    }
 
-        PLAT_UI8 pub_key_size = stse_ecc_info_table[key_type].public_key_size;
-        PLAT_UI8 pub_key[pub_key_size];
-        PLAT_UI8 signature_size = stse_ecc_info_table[key_type].signature_size;
-        PLAT_UI8 signature[signature_size];
+    PLAT_UI8 pub_key_size = stse_ecc_info_table[key_type].public_key_size;
+    PLAT_UI8 pub_key[pub_key_size];
+    PLAT_UI8 signature_size = stse_ecc_info_table[key_type].signature_size;
+    PLAT_UI8 signature[signature_size];
 #ifdef STSE_CONF_USE_COMPANION
-        PLAT_UI8 signature_validity;
+    PLAT_UI8 signature_validity;
 #endif
 
-        /* Extract and format the public key from the certificate */
-        if (cert->SignatureAlgorithm == SIG_EDDSA_ED25519) {
-            memcpy(pub_key, cert->PubKey.pX, pub_key_size);
+    /* Extract and format the public key from the certificate */
+    if (cert->SignatureAlgorithm == SIG_EDDSA_ED25519) {
+        memcpy(pub_key, cert->PubKey.pX, pub_key_size);
+    } else {
+        memcpy(pub_key, cert->PubKey.pX, (pub_key_size >> 1));
+        if (*cert->pPubKey_point_representation_id == 0x04) {
+            memcpy(pub_key + (pub_key_size >> 1), cert->PubKey.pY, (pub_key_size >> 1));
         } else {
-            memcpy(pub_key, cert->PubKey.pX, (pub_key_size >> 1));
-            if (*cert->pPubKey_point_representation_id == 0x04) {
-                memcpy(pub_key + (pub_key_size >> 1), cert->PubKey.pY, (pub_key_size >> 1));
-            } else {
 #ifdef STSE_CONF_USE_COMPANION
-                if (stsafe_x509_parser_companion_handler != NULL) {
-                    stsafea_ecc_decompress_public_key(
-                        stsafe_x509_parser_companion_handler,
-                        key_type,
-                        *cert->pPubKey_point_representation_id,
-                        pub_key,
-                        pub_key + (pub_key_size >> 1));
-                } else
+            if (stsafe_x509_parser_companion_handler != NULL) {
+                stsafea_ecc_decompress_public_key(
+                    stsafe_x509_parser_companion_handler,
+                    key_type,
+                    *cert->pPubKey_point_representation_id,
+                    pub_key,
+                    pub_key + (pub_key_size >> 1));
+            } else
 #endif
-                {
-                    return STSE_CERT_UNSUPPORTED_FEATURE;
-                }
+            {
+                return STSE_CERT_UNSUPPORTED_FEATURE;
             }
         }
+    }
 
-        /* Format the signature */
-        memcpy(signature, signatureR, (signature_size >> 1));
-        memcpy(signature + (signature_size >> 1), signatureS, (signature_size >> 1));
+    /* Format the signature */
+    memcpy(signature, signatureR, (signature_size >> 1));
+    memcpy(signature + (signature_size >> 1), signatureS, (signature_size >> 1));
 
 #ifdef STSE_CONF_USE_COMPANION
-        if (stsafe_x509_parser_companion_handler != NULL) {
-            /* Verify the signature using STSAFE */
-            ret = stse_ecc_verify_signature(
-                stsafe_x509_parser_companion_handler,
-                key_type,
-                pub_key,
-                signature,
-                (PLAT_UI8 *)digest,
-                (PLAT_UI16)digestSize,
-                1, /* Message is hashed */
-                &signature_validity);
+    if (stsafe_x509_parser_companion_handler != NULL) {
+        /* Verify the signature using STSAFE */
+        ret = stse_ecc_verify_signature(
+            stsafe_x509_parser_companion_handler,
+            key_type,
+            pub_key,
+            signature,
+            (PLAT_UI8 *)digest,
+            (PLAT_UI16)digestSize,
+            1, /* Message is hashed */
+            &signature_validity);
 
-            if ((ret == STSE_OK) && (signature_validity == 1)) {
-                return (STSE_OK);
-            }
-        } else
-#endif
-        {
-            ret = stse_platform_ecc_verify(
-                key_type,
-                pub_key,
-                (PLAT_UI8 *)digest,
-                (PLAT_UI16)digestSize,
-                signature);
-
-            if (ret == STSE_OK) {
-                return (STSE_OK);
-            }
+        if ((ret == STSE_OK) && (signature_validity == 1)) {
+            return (STSE_OK);
         }
+    } else
+#endif
+    {
+        ret = stse_platform_ecc_verify(
+            key_type,
+            pub_key,
+            (PLAT_UI8 *)digest,
+            (PLAT_UI16)digestSize,
+            signature);
 
-        return (STSE_CERT_INVALID_SIGNATURE);
+        if (ret == STSE_OK) {
+            return (STSE_OK);
+        }
+    }
+
+    return (STSE_CERT_INVALID_SIGNATURE);
 }
 
 stse_ecc_key_type_t stse_certificate_get_key_type(const stse_certificate_t *cert) {
-        switch (cert->EllipticCurve) {
+    switch (cert->EllipticCurve) {
 #ifdef STSE_CONF_ECC_NIST_P_256
-        case EC_P256:
-            return STSE_ECC_KT_NIST_P_256;
+    case EC_P256:
+        return STSE_ECC_KT_NIST_P_256;
 #endif
 #ifdef STSE_CONF_ECC_NIST_P_384
-        case EC_P384:
-            return STSE_ECC_KT_NIST_P_384;
+    case EC_P384:
+        return STSE_ECC_KT_NIST_P_384;
 #endif
 #ifdef STSE_CONF_ECC_NIST_P_521
-        case EC_P521:
-            return STSE_ECC_KT_NIST_P_521;
+    case EC_P521:
+        return STSE_ECC_KT_NIST_P_521;
 #endif
 #ifdef STSE_CONF_ECC_BRAINPOOL_P_256
-        case EC_bp256r1:
-            return STSE_ECC_KT_BP_P_256;
+    case EC_bp256r1:
+        return STSE_ECC_KT_BP_P_256;
 #endif
 #ifdef STSE_CONF_ECC_BRAINPOOL_P_384
-        case EC_bp384r1:
-            return STSE_ECC_KT_BP_P_384;
+    case EC_bp384r1:
+        return STSE_ECC_KT_BP_P_384;
 #endif
 #ifdef STSE_CONF_ECC_BRAINPOOL_P_512
-        case EC_bp512r1:
-            return STSE_ECC_KT_BP_P_512;
+    case EC_bp512r1:
+        return STSE_ECC_KT_BP_P_512;
 #endif
 #ifdef STSE_CONF_ECC_EDWARD_25519
-        case EC_Ed25519:
-            return STSE_ECC_KT_ED25519;
+    case EC_Ed25519:
+        return STSE_ECC_KT_ED25519;
 #endif
-        default:
-            return STSE_ECC_KT_INVALID;
-        }
+    default:
+        return STSE_ECC_KT_INVALID;
+    }
 }
 
 stse_hash_algorithm_t stse_certificate_get_sig_hash_algo(const stse_certificate_t *cert) {
-        switch (cert->SignatureAlgorithm) {
+    switch (cert->SignatureAlgorithm) {
 #ifdef STSE_CONF_HASH_SHA_1
-        case SIG_ECDSA_SHA1:
-            return STSE_SHA_1;
+    case SIG_ECDSA_SHA1:
+        return STSE_SHA_1;
 #endif
 #ifdef STSE_CONF_HASH_SHA_224
-        case SIG_ECDSA_SHA224:
-            return STSE_SHA_224;
+    case SIG_ECDSA_SHA224:
+        return STSE_SHA_224;
 #endif
 #ifdef STSE_CONF_HASH_SHA_256
-        case SIG_ECDSA_SHA256:
-            return STSE_SHA_256;
-        case SIG_EDDSA_ED25519:
-            return STSE_SHA_256;
+    case SIG_ECDSA_SHA256:
+        return STSE_SHA_256;
+    case SIG_EDDSA_ED25519:
+        return STSE_SHA_256;
 #endif
 #ifdef STSE_CONF_HASH_SHA_384
-        case SIG_ECDSA_SHA384:
-            return STSE_SHA_384;
+    case SIG_ECDSA_SHA384:
+        return STSE_SHA_384;
 #endif
 #ifdef STSE_CONF_HASH_SHA_512
-        case SIG_ECDSA_SHA512:
-            return STSE_SHA_512;
+    case SIG_ECDSA_SHA512:
+        return STSE_SHA_512;
 #endif
-        default:
-            return STSE_SHA_INVALID;
-        }
+    default:
+        return STSE_SHA_INVALID;
+    }
 }
