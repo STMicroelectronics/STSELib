a329b2113a3f8b467619b3d992e4dee4779c46c1
Parminder Singh <parminder.sh93@gmail.com>
2026-03-31 10:59:46 +0100

    Use indexed ECC info table
    Rework `stse_ecc_info_table` to use explicit indexing via
    `stse_ecc_key_type_t` values instead of relying on positional ordering.
    This improves readability, maintains a direct mapping between key types
    and table entries, and prevents potential issues caused by
    configuration-dependent ordering.

--- core/stse_generic_typedef.h
+++ core/stse_generic_typedef.h
@@ -71,6 +71,8 @@
 /*!
  * \enum stse_ecc_key_type_t
  * \brief STSE ECC key type
+ *
+ * \note Do not assign any value to these enumerators, as they are used as indices in the `stse_ecc_info_table` array.
  */
 typedef enum stse_ecc_key_type_t {
 #ifdef STSE_CONF_ECC_NIST_P_256
