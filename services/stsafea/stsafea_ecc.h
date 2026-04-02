795942c3ecd698e7327e0b88a09e1dcb7a2c6dba
Parminder Singh <parminder.sh93@gmail.com>
2026-04-02 20:52:39 +0100

    Make input buffers const in ECC verify
    Mark the read-only buffers as `const` in `xxx_ecc_verify_signature()` to
    reflect that the input buffer is not modified. This improves
    const-correctness and clarifies the API contract.

--- services/stsafea/stsafea_ecc.h
+++ services/stsafea/stsafea_ecc.h
@@ -101,9 +101,9 @@ stse_ReturnCode_t stsafea_stop_volatile_KEK_session(
 stse_ReturnCode_t stsafea_ecc_verify_signature(
     stse_Handler_t *pSTSE,
     stse_ecc_key_type_t key_type,
-    PLAT_UI8 *pPublic_key,
-    PLAT_UI8 *pSignature,
-    PLAT_UI8 *pMessage,
+    const PLAT_UI8 *pPublic_key,
+    const PLAT_UI8 *pSignature,
+    const PLAT_UI8 *pMessage,
     PLAT_UI16 message_length,
     PLAT_UI8 eddsa_variant,
     PLAT_UI8 *pSignature_validity);
