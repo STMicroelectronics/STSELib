be1bd54dd5d8372fdf283343ab446bd9088bcd5c
Christophe Delaunay <christophe.delaunay@st.com>
2026-08-26 15:14:17 +0200

    [fix][services] fix potential infinite loop when transmitting or filling frame elements

--- services/stsafea/stsafea_frame_transfer.c
+++ services/stsafea/stsafea_frame_transfer.c
@@ -86,7 +86,7 @@ stse_ReturnCode_t stsafea_frame_transmit(stse_Handle_t *pSTSE, stse_frame_t *pFr
 
         if (ret == STSE_OK) {
             pCurrent_element = pFrame->first_element;
-            while (pCurrent_element != pFrame->last_element) {
+            while (pCurrent_element != pFrame->last_element && pCurrent_element != NULL) {
                 ret = pSTSE->io.BusSendContinue(
                     pSTSE->io.busID,
                     pSTSE->io.Devaddr,
@@ -325,7 +325,7 @@ stse_ReturnCode_t stsafea_frame_receive(stse_Handle_t *pSTSE, stse_frame_t *pFra
 
         /* - Perform frame element reception and populate local RSP Frame */
         pCurrent_element = pFrame->first_element->next;
-        while (pCurrent_element != pFrame->last_element) {
+        while (pCurrent_element != pFrame->last_element && pCurrent_element != NULL) {
             if (received_length < pCurrent_element->length) {
                 pCurrent_element->length = received_length;
             }
