4edd5877dc3830c978b4d6c7d1eddaa37eed1ba8
Christophe Delaunay <christophe.delaunay@st.com>
2026-08-26 15:14:17 +0200

    [fix][services] fix potential infinite loop when transmitting or filling frame elements

--- services/stsafel/stsafel_frame_transfer.c
+++ services/stsafel/stsafel_frame_transfer.c
@@ -83,7 +83,7 @@ stse_ReturnCode_t stsafel_frame_transmit(stse_Handle_t *pSTSE, stse_frame_t *pFr
 
         if (ret == STSE_OK) {
             pCurrent_element = pFrame->first_element;
-            while (pCurrent_element != pFrame->last_element) {
+            while (pCurrent_element != pFrame->last_element && pCurrent_element != NULL) {
                 ret = pSTSE->io.BusSendContinue(
                     pSTSE->io.busID,
                     pSTSE->io.Devaddr,
@@ -283,7 +283,7 @@ stse_ReturnCode_t stsafel_i2c_frame_receive(stse_Handle_t *pSTSE, stse_frame_t *
 
     /* - Perform frame element reception and populate local RSP Frame */
     pCurrent_element = pFrame->first_element->next;
-    while (pCurrent_element != pFrame->last_element) {
+    while (pCurrent_element != pFrame->last_element && pCurrent_element != NULL) {
         if (received_length < pCurrent_element->length) {
             pCurrent_element->length = received_length;
         }
@@ -422,7 +422,7 @@ stse_ReturnCode_t stsafel_st1wire_frame_receive(stse_Handle_t *pSTSE, stse_frame
 
     /* - Perform frame element reception and populate local RSP Frame */
     pCurrent_element = pFrame->first_element->next;
-    while (pCurrent_element != pFrame->last_element) {
+    while (pCurrent_element != pFrame->last_element && pCurrent_element != NULL) {
         if (received_length < pCurrent_element->length) {
             pCurrent_element->length = received_length;
         }
