0ff0bace56131ab2b6929b29d81d012859fe6c8b
Jerome Semette <jerome.semette@st.com>
2026-09-25 16:34:59 +0200

    [fix] fix regression in stse_aes.c

--- services/stsafea/stsafea_sessions.c
+++ services/stsafea/stsafea_sessions.c
@@ -185,7 +185,7 @@ stse_ReturnCode_t stsafea_session_frame_encrypt(stse_session_t *pSession,
       || (pFrame == NULL)
       || (pEnc_payload_element == NULL)
       || (pEnc_payload_element->length < (pFrame->length - pFrame->first_element->length + (16 -
-                                       (pFrame->length - pFrame->first_element->length) % 16))))
+                                          (pFrame->length - pFrame->first_element->length) % 16))))
   {
     return (STSE_SERVICE_INVALID_PARAMETER);
   }
